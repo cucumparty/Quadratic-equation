3006b29e64656e13c53672b3efd35865bb26ba91
BatyaPng <89447620+BatyaPng@users.noreply.github.com>
2021-08-30 22:47:11 +0300

    Update main.c

--- main.c
+++ main.c
@@ -16,12 +16,14 @@ int main() {
     system("chcp 65001");
 
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-    int number_roots = 0;
+    int number_roots = -2;
 
     input_function(&a, &b, &c);
     solve(a, b, c, &x1, &x2, &number_roots);
     output_function(number_roots, x1, x2);
-    
+
+    getchar();
+
     return 0;
 }
 
@@ -42,7 +44,7 @@ void input_function(double *a, double *b, double *c) {
 void solve(double a, double b, double c, double *x1, double *x2, int *number_roots) {
     if (a != 0) {
         quadratic_solve(a, b, c, x1, x2, number_roots);
-    } else {
+    } else if (a == 0 & b != 0) {
         linear_solve(b, c, x1, number_roots);
     }
 }
@@ -58,6 +60,8 @@ void quadratic_solve(double a, double b, double c, double *x1, double *x2, int *
     } else if (D == 0) {
         *x1 = (-b + sqrt(D)) / 2 * a;
         *number_roots = 1;
+    } else{
+        *number_roots = 0;
     }
 }
 
@@ -67,12 +71,21 @@ void linear_solve(double b, double c, double *x1, int *number_roots) {
 }
 
 void output_function(int number_roots, double x1, double x2) {
-    if (number_roots == 2) {
-        printf("Корни уравнения равны %.3lf; %.3lf", x1, x2);
-    } else if (number_roots == 1) {
-        printf("Корень уравнения равен %.3lf", x1);
-    } else if (number_roots == 0) {
-        printf("Уравнение не имеет действительных корней");
-    } else
-        printf("Ты ввёл коэффициенты, определяющие линейное уравнение. Корень данного уравнениея равен %.3lf\n", x1);
+    switch (number_roots) {
+        case 2:
+            printf("Корни уравнения равны %.3lf; %.3lf", x1, x2);
+            break;
+        case 1:
+            printf("Корень уравнения равен %.3lf", x1);
+            break;
+        case 0:
+            printf("Уравнение не имеет действительных корней");
+            break;
+        case -1:
+            printf("Ты ввёл коэффициенты, определяющие линейное уравнение. Корень данного уравнениея равен %.3lf\n",
+                   x1);
+            break;
+        default:
+            printf("Ты ввёл коэффициенты, равные нулю, поэтому решением являеются все числа");
+    }
 }
